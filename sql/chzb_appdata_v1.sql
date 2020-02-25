d15d9b3fb7b76c52d5c66798e2246bc97107a74e
luo2888 <kwangaho@gmail.com>
2020-02-25 16:06:23 +0800

    Update

--- sql/chzb_appdata_v1.sql
+++ sql/chzb_appdata_v1.sql
@@ -42,6 +42,9 @@ CREATE TABLE `chzb_appdata` (
   `autoupdate` int(11) DEFAULT '1',
   `randkey` varchar(100) DEFAULT '827ccb0eea8a706c4c34a16891f84e7b',
   `updateinterval` int(11) DEFAULT '15',
+  `up_size` varchar(16) DEFAULT NULL,
+  `up_sets` int(11) NOT NULL,
+  `ip_text` varchar(1024) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
@@ -49,4 +52,4 @@ CREATE TABLE `chzb_appdata` (
 -- 转存表中的数据 chzb_appdata
 --
 
-INSERT INTO `chzb_appdata` VALUES('1','2','2.0','1','http://tv.luo2888.cn/data.php','http://download.luo2888.cn/software/%E8%82%A5%E7%B1%B3TV/%E8%82%A5%E7%B1%B3TV_V2.0.apk','','30','100','1','http://tv.luo2888.cn/images/tv.png','1','30','请输入授权码登录，或联系公众号客服@luo2888的工作室。','账号已到期，请联系公众号客服@luo2888的工作室续费。','账号已禁用，请联系公众号客服@luo2888的工作室。','正在连接，请稍后 ...','2','365','欢迎关注微信公众号@luo2888的工作室','1','827ccb0eea8a706c4c34a16891f84e7b','10');
+INSERT INTO `chzb_appdata` VALUES('1','2','2.0','1','http://127.0.0.1','http://127.0.0.1/1.apk','','30','100','1','http://127.0.0.1/bg.png','1','30','请输入授权码登录，或联系公众号客服@luo2888的工作室。','账号已到期，请联系公众号客服@luo2888的工作室续费。','账号已禁用，请联系公众号客服@luo2888的工作室。','正在连接，请稍后 ...','2','365','欢迎关注微信公众号@luo2888的工作室','1','827ccb0eea8a706c4c34a16891f84e7b','10','','1','');
